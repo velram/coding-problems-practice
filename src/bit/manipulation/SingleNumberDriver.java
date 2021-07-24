36492b091254f20cdd5ec76bdce7a2a4bcd5af3a
Velmurugan Moorthy <velmurugan1993@gmail.com>
2021-07-24 15:22:38 +0530

    Vel | SingleNumber - problem - solved

---
+++ src/bit/manipulation/SingleNumberDriver.java
@@ -0,0 +1,28 @@
+package bit.manipulation;
+
+/**
+ * @author Velmurugan Moorthy
+ * @date 23.07.2021
+ * Finding a single number in a collection of duplicate (2 times) 
+ */
+public class SingleNumberDriver {
+    public static void main(String[] args) {
+        SingleNumberFinder singleNumberFinder = new SingleNumberFinder();
+        int[] inputs = {1, 2, 2, 3, 1};
+        int result = singleNumberFinder.findSingleNumber(inputs);
+        System.out.println("Result is : " + result);
+    }
+}
+
+class SingleNumberFinder {
+    public int findSingleNumber(int[] inputs){
+        int arrayLength = inputs.length;
+        int result = inputs[0];
+
+        for(int index = 1; index < arrayLength; index++) {
+            result = result ^ inputs[index];
+        }
+
+        return result;
+    }
+}
