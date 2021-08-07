b41900f95f5b5d47a2e8750f4dc5967efc9684ef
Velmurugan Moorthy <velmurugan.moorthy@protonmail.com>
2021-08-07 13:29:23 +0530

    Vel | Day-7 | Arrays - Good pair finder

--- src/com/scaler/practice/array/GoodPairDriver.java
+++ src/com/scaler/practice/array/GoodPairDriver.java
@@ -1,5 +1,8 @@
 package com.scaler.practice.array;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * @author Velmurugan Moorthy
  * @date 06.08.2021
@@ -86,7 +89,75 @@ package com.scaler.practice.array;
 public class GoodPairDriver {
     public static void main(String[] args) {
 
+//Test case #1 Sample input from question - PASS
+//        int[] input = {1,2,3,4};
+//        int target = 7;
+
+//Test case #2 Sample input from question - PASS
+//        int[] input = {1,2,4};
+//        int target = 4;
+
+//Test case #3 Sample input from question - PASS
+//        int[] input = {1,2,2};
+//        int target = 4;
+
+//Test case #4 single element array & not matching target - PASS
+//        int[] input = {1};
+//        int target = 2;
+
+//Test case #5 two element array - No pair - PASS
+//        int[] input = {1, 2};
+//        int target = 2;
+
+//Test case #6 two element array - pair exists - PASS
+//        int[] input = {1, 2};
+//        int target = 3;
+
+//Test case #7 No pair - i!=j test case - PASS
+//        int[] input = {1,2,3};
+//        int target = 6;
+
+//Test case #8 No pair - i!=j test case - PASS
+        int[] input = {1,0,3,1,0, 3};
+        int target = 6;
+
+//Test case #
+//        int[] input = {};
+//        int target = 0;
+
+
+        GoodPairFinder goodPairFinder = new GoodPairFinder();
+        int pairExists = goodPairFinder.findGoodPairs(input, target);
+
+        System.out.println(" Pair exists : " + pairExists);
     }
 }
 
+class GoodPairFinder {
+    public int findGoodPairs(int[] input, int target){
+
+        //Edge case - Single element array
+        if(input.length < 2){
+            return 0;
+        }
+
+        if(input.length == 2){
+            return (input[0] + input[1] == target) ? 1 : 0;
+        }
+
+        List<Integer> inputElement = new ArrayList<>();
+
+        for(int loopIndex = 0; loopIndex < input.length; loopIndex++){
+          int difference = target - input[loopIndex];
+
+          if(inputElement.contains(difference)){
+              return 1;
+          }
+          else {
+              inputElement.add(input[loopIndex]);
+          }
+        }
+            return 0;
+    }
+}
 
