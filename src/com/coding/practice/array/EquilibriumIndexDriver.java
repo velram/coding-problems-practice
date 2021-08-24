37b377a40d52ea61665e342505d94e299fb0d204
Velmurugan Moorthy <velmurugan.moorthy@protonmail.com>
2021-08-24 09:06:11 +0530

    Vel - Equilibrium index

---
+++ src/com/coding/practice/array/EquilibriumIndexDriver.java
@@ -0,0 +1,131 @@
+package com.coding.practice.array;
+
+
+/**
+ * @author Velmurugan Moorthy
+ * @date 24.08.2021
+ *
+ * Problem name : Equilibrium index of an array
+ *
+ * Problem Description
+ *
+ * You are given an array A of integers of size N.
+ *
+ * Your task is to find the equilibrium index of the given array
+ *
+ * Equilibrium index of an array is an index such that the sum of elements at lower indexes is equal to the sum of elements at higher indexes.
+ *
+ * NOTE:
+ *
+ * Array indexing starts from 0.
+ * If there is no equilibrium index then return -1.
+ * If there are more than one equilibrium indexes then return the minimum index.
+ *
+ *
+ *
+ * Problem Constraints
+ *
+ * 1<=N<=1e5
+ * -1e5<=A[i]<=1e5
+ *
+ *
+ *
+ * Input Format
+ * First arugment contains an array A .
+ *
+ *
+ * Output Format
+ * Return the equilibrium index of the given array. If no such index is found then return -1.
+ *
+ *
+ * Example Input
+ * Input 1:
+ *
+ * A=[-7, 1, 5, 2, -4, 3, 0]
+ *
+ * Input 2:
+ *
+ * A=[1,2,3]
+ *
+ *
+ *
+ * Example Output
+ * Output 1:
+ *
+ * 3
+ *
+ * Output 2:
+ *
+ * -1
+ *
+ *
+ *
+ * Example Explanation
+ * Explanation 1:
+ *
+ * 3 is an equilibrium index, because:
+ * A[0] + A[1] + A[2] = A[4] + A[5] + A[6]
+ *
+ * Explanation 1:
+ *
+ * There is no such index.
+ *
+ */
+public class EquilibriumIndexDriver {
+    public static void main(String[] args) {
+
+//        int[] input = {-7, 1, 5, 2, -4, 3, 0};//Test case #1 Happy flow - pass
+//        int[] input = {-5,3,-3};//Test case #2 first index is equilibrium - PASS
+//        int[] input = {-5,5,2};//Test case #3 last index is equilibrium - PASS
+//        int[] input = {-8, 3, 6, 10, -4, 3, 2};//Test case #4 middle index is equilibrium
+        int[] input = {-8, 3, 4, 10, -4, 3, 2};//Test case #5 No equilibrium found
+//        int[] input = {};//Test case #
+//        int[] input = {};//Test case #
+//        int[] input = {};//Test case #
+//        int[] input = {};//Test case #
+
+
+        EquilibriumIndexFinder equilibriumIndexFinder = new EquilibriumIndexFinder();
+        int equilibriumIndex = equilibriumIndexFinder.findEquilibriumIndex(input);
+        System.out.println("Equilibrium index : " + equilibriumIndex);
+
+    }
+}
+
+class EquilibriumIndexFinder {
+    public int findEquilibriumIndex(int[] input){
+
+        int inputSize = input.length;
+        long[] prefixSum = findPrefixSum(input);
+
+        //Corner case : first index is equilibrium index :
+
+        if(0 == (prefixSum[inputSize - 1] - prefixSum[0])){
+            return 0;
+        }
+
+        for(int loopIndex = 1; loopIndex < inputSize; loopIndex++){
+            long leftSum = prefixSum[loopIndex - 1];
+            long rightSum = prefixSum[inputSize - 1] - prefixSum[loopIndex];
+
+            if(leftSum == rightSum){
+                return loopIndex;
+            }
+        }
+
+        return -1;
+    }
+
+    public long[] findPrefixSum(int[] input){
+
+        long[] prefixSum = new long[input.length];
+
+        prefixSum[0] = input[0];
+
+        for(int loopIndex = 1; loopIndex < input.length; loopIndex++){
+            prefixSum[loopIndex] += (prefixSum[loopIndex - 1]  + input[loopIndex]);
+        }
+
+        return prefixSum;
+    }
+}
