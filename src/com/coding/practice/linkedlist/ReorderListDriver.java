632261dba1064963a86711fcee794faee22f9523
Velmurugan Moorthy <vel@Vels-MacBook-Pro.local>
2022-02-24 00:06:59 +0530

    Vel - LinkedList - ReOrder

--- src/com/coding/practice/linkedlist/ReorderListDriver.java
+++ src/com/coding/practice/linkedlist/ReorderListDriver.java
@@ -67,12 +67,59 @@ package com.coding.practice.linkedlist;
 public class ReorderListDriver {
     public static void main(String[] args) {
 
+        LinkedListReorderTools linkedListReorderTools = new LinkedListReorderTools();
+
+        LinkedListNode head = new LinkedListNode(1);
+        head.next = new LinkedListNode(2);
+        head.next.next =  new LinkedListNode(3);
+        head.next.next.next =  new LinkedListNode(4);
+        head.next.next.next.next =  new LinkedListNode(5);
+        head.next.next.next.next.next =  new LinkedListNode(6);
+        head.next.next.next.next.next.next =  new LinkedListNode(7);
+        //head.next.next.next.next.next.next.next =  new LinkedListNode(8);
+
+        LinkedList linkedList = new LinkedList();
+        System.out.println("LinkedList before reordering : ");
+        linkedList.printLinkedList(head);
+
+        linkedListReorderTools.deleteAtEnd(head);
+        System.out.println("LinkedList after deleting : ");
+        linkedList.printLinkedList(head);
+
     }
 }
 
-class ListReorderTools{
-    public LinkedListNode reorderList(){
 
-        return null;
+class LinkedListReorderTools {
+    public LinkedListNode reorderList(LinkedListNode head){
+
+        LinkedListNode reorderedListHead = null;
+
+        LinkedListNode[] result = deleteAtEnd(head);
+        
+
+        return reorderedListHead;
+    }
+
+    public LinkedListNode[] deleteAtEnd(LinkedListNode head){
+
+        LinkedListNode current = head;
+        LinkedListNode deletedNode = null;
+        LinkedListNode[] result = new LinkedListNode[2];
+
+        if(head.next == null){
+            result[0] = null;
+            result[1] = head;
+            return result;
+        }
+
+        while(current.next.next != null){
+         current = current.next;
+        }
+        result[1] = current.next.next;
+        current.next = null;
+        result[0] = head;
+
+        return result;
     }
 }
